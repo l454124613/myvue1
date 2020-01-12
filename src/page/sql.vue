4accd91dfe0415f3c7299f2f0c617b5bac9060e2
lxch <lixuechengde@163.com>
2020-01-12 16:51:58 +0800

    修改sql表单

--- src/page/sql.vue
+++ src/page/sql.vue
@@ -8,25 +8,50 @@
   >
     <el-tab-pane label="数据库配置" name="first" :closable="false">
       <el-button size="small" @click="dialogFormVisible = true">添加数据库</el-button>
-      
 
-<el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-  <el-form :model="form">
-    <el-form-item label="活动名称" :label-width="formLabelWidth">
-      <el-input v-model="form.name" autocomplete="off"></el-input>
-    </el-form-item>
-    <el-form-item label="活动区域" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-  </el-form>
-  <div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-  </div>
-</el-dialog>
+      <el-dialog title="数据库配置" :visible.sync="dialogFormVisible" :before-close="show">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline" :label-position="'left'">
+          <el-form-item label="连接名称">
+            <el-input v-model="formInline.name" class="inp" placeholder="连接名称"></el-input>
+          </el-form-item>
+          <el-form-item label="连接类型">
+            <el-select v-model="formInline.region" placeholder="数据库类型">
+              <el-option label="Mysql" value="mysql"></el-option>
+              <el-option label="Sqlserver" value="sqlserver"></el-option>
+              <el-option label="Sqlite" value="sqlite"></el-option>
+              <el-option label="Mongodb" value="mongodb"></el-option>
+              <el-option label="Redis" value="redis"></el-option>
+
+            </el-select>
+            
+          </el-form-item>
+          <br>
+          <el-form-item label="连接地址">
+            <el-input v-model="formInline.ip" class="inp" placeholder="ip地址"></el-input>
+          </el-form-item>
+          <el-form-item label="连接端口">
+            <el-input v-model="formInline.port" class="inp" placeholder="端口"></el-input>
+          </el-form-item>
+          <el-form-item label="登录名称">
+            <el-input v-model="formInline.user" class="inp" placeholder="登录名"></el-input>
+          </el-form-item>
+          <el-form-item label="登录密码">
+            <el-input v-model="formInline.passwd" class="inp" placeholder="密码"></el-input>
+          </el-form-item>
+          <el-form-item label="数据库名">
+            <el-input v-model="formInline.db" class="inp" placeholder="数据库"></el-input>
+          </el-form-item>
+          <!-- <el-form-item>
+            <el-button type="success" @click="onSubmit">连接测试</el-button>
+          </el-form-item> -->
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+           <el-button type="success" @click="dialogFormVisible = false">测试</el-button> 
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+         
+        </div>
+      </el-dialog>
       <el-button size="small" @click="addTab(editableTabsValue)">add tab</el-button>
       <el-button size="small" @click="removeAllTab()">remove all tab</el-button>
     </el-tab-pane>
@@ -54,22 +79,23 @@ export default {
       editableTabs: [],
       tabIndex: 0,
       dialogFormVisible: false,
-      form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
-        formLabelWidth: '120px'
+      formInline: {
+        name: "",
+        region: "",
+        ip:"",
+        port:"",
+        user:"",
+        passwd:"",
+        db:""
+      }
     };
   },
   methods: {
     handleClick(tab, event) {
       console.log(event);
+    },show(a){alert(a)},
+    onSubmit() {
+      console.log("submit!");
     },
     addTab(targetName) {
       let newTabName = ++this.tabIndex + "";
@@ -110,5 +136,8 @@ export default {
 #tabs {
   margin-left: 8px;
   margin-right: 8px;
+}
+.inp{
+width: 193px
 }
 </style>
